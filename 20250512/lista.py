c88a5e71af3b88bdecc40379f78e5a632b793557
ainaravazquez <vazquezainarajazmin@gmail.com>
2025-05-19 19:35:40 -0300

    add class 20250507

---
+++ 20250512/lista.py
@@ -0,0 +1,5 @@
+# Crear una lista nueva con los elementos potenciados por sí mismos
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+numeros_potenciados = [ x**x for x in numbers]
+print (numeros_potenciados) 
---
+++ 20250525/paramateros.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+def mod (dividendo, divisor):
+    return dividendo % divisor
+
+dividendo = 10
+divisor = 0 
+
+print (f'El resultado de {dividendo} % {divisor} es: {mod(dividendo, divisor)}')
